1e93cf97d3ba33db7a07d507c8bcc3a1f5afff52
_vishwaraj <vishwarajchundawat008@gmail.com>
2025-07-24 18:51:39 +0530

    now node mailer is working

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const validator = require('validator');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://yourportfolio.com', // restrict to your domain
+  origin: 'https://yourportfolio.com'|| 'file:///Users/macbook/Desktop/portfolio/index.html' // restrict to your domain
 }));
 
 const transporter = nodemailer.createTransport({
